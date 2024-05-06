136fb287c5557ccf5f867bf98f07fa4c53cf0ddd
Chloe ONeil <chloesporri@yahoo.com>
2024-05-05 17:51:42 -0700

    add boiler cards into BlackCardDeck

---
+++ src/Deck/BlackCardDeck.js
@@ -0,0 +1,14 @@
+
+const blackCardOptions = [
+  {
+    text: "Fill in the blank: ___ is the best way to relax.",
+  },
+  {
+    text: "What's that smell?",
+  },
+  {
+    text: "Why can't I sleep at night?",
+  },
+];
+
+export default blackCardOptions;
