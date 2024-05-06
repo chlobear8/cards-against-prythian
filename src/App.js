48ce85d5481aefdd474af685b25827d02568e3d3
Chloe ONeil <chloesporri@yahoo.com>
2024-05-05 17:48:10 -0700

    test on whiteCard display works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import BlackCard from './Components/BlackCard';
+import WhiteCard from './Components/WhiteCard';
 
 export default function App() {
   return (
     <div className="App">
       <BlackCard text="Fill in the blank: ___ is the best way to relax." />
+      <WhiteCard text="Taking a long walk" />
+      <WhiteCard text="Reading a good book" />
     </div>
   );
 };
---
+++ src/Components/WhiteCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function WhiteCard({ text }) {
+  return (
+    <div className="white-card">
+      <p>{text}</p>
+    </div>
+  );
+};
+
+
