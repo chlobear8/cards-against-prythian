a1eb8e92107c96ccceaf7d6800291f5c3c0773ee
Chloe ONeil <chloesporri@yahoo.com>
2024-05-05 17:54:25 -0700

    add blackCardOptions to App and test passed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import BlackCard from './Components/BlackCard';
 import WhiteCard from './Components/WhiteCard';
+import blackCardOptions from './Deck/BlackCardDeck';
 
 export default function App() {
+  const randomBlackCard = blackCardOptions[Math.floor(Math.random() * blackCardOptions.length)];
   return (
     <div className="App">
-      <BlackCard text="Fill in the blank: ___ is the best way to relax." />
+      <BlackCard text={randomBlackCard.text} />
       <WhiteCard text="Taking a long walk" />
       <WhiteCard text="Reading a good book" />
     </div>
