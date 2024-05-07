bee86dbc981b70602818434553071260b71e4e46
Chloe ONeil <chloesporri@yahoo.com>
2024-05-06 18:45:12 -0700

    create selected option WIP

--- src/Components/WhiteCard.js
+++ src/Components/WhiteCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default function WhiteCard({ text }) {
+export default function WhiteCard({ text, selectWinner, onClick, selected }) {
   return (
-    <div className="white-card">
+    <div className={`white-card ${selected ? 'selected' : ''}`} onClick={onClick}>
       <p>{text}</p>
+      <button onClick={selectWinner}>Select as the Winner</button>
     </div>
   );
 };
